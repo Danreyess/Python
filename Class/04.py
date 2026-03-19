bd1e7658d11b1099b5c1beba84e45b70794f29a3
Dani <dani@86:77:27:10:47:2c.home>
2026-03-18 23:15:10 -0400

    04-class

---
+++ Class/04.py
@@ -0,0 +1,7 @@
+class Character:
+    def __init__(self, name, level, health):
+        self.name = name
+        self.level = level
+        self.health = health
+        
+    def showBalance(self):
