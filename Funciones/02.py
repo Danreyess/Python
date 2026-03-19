e6fc5b7f24ec801607cb3fc93d76aad2a2ce1072
Dani <dani@86:77:27:10:47:2c.home>
2026-03-18 23:28:58 -0400

    New function 02.py

---
+++ Funciones/02.py
@@ -0,0 +1,6 @@
+def calculate_tax(net_salary, tax = 0.13):
+    tax_rate = (net_salary/100)*tax
+    print(f"Tax rate: {tax_rate} of your salary {net_salary}")
+    return tax_rate
+
+calculate_tax(200)
